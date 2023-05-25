d5ecdebb8783701de86f9f282aca80f34868bd24
ChahrazadBouragba <chbouragba25@gmail.com>
2023-05-25 16:26:47 +0100

    added reactions

--- BMI-Calculator/script.js
+++ BMI-Calculator/script.js
@@ -1,5 +1,6 @@
 
 
+
 // Accessing the input elements and the result element from the DOM
 const weightInput = document.getElementById('weight')
 const heightInput = document.getElementById('height')
@@ -12,91 +13,65 @@ console.log(weight)
 console.log(height)
 
 
-
-
 //? BMI = weight (kg) / height (m)²
 //* Function to calculate BMI given weight (kg) and height (cm)
 const calculateBmi = () => {
-    // Get the weight and height inputs from the DOM & convert it into a number
-    const weight = Number(weightInput.value);
-    const height = Number(heightInput.value);
-    // console.log(weight)
-    // console.log(height)
 
-    //* guard clause
-    // if input is 1 or less simply return
-    if (weight <= 1 && height <= 1) {
-        console.log(weight, `weight is 1 or less than 1 so I'm gonna stop`)
-        console.log(height, `height is 1 or less than 1 so I'm gonna stop`)
-        // alert('Enter a valid number')
-        throw 'Enter a valid number'
-        return
+
+    const stickerContainer = document.getElementById('sticker-container');
+const result = document.getElementById('result');
+const rangeElement = document.getElementById('range');
+
+stickerContainer.addEventListener('click', () => {
+    try {
+        const weight = Number(weightInput.value);
+        const height = Number(heightInput.value);
+      
+       // perform BMI calculation and display result
+        if (height && weight) {
+            const heightm = height / 100; 
+            const bmi = weight / (heightm * heightm);
+            const roundedBmi = bmi.toFixed(1);
+            result.innerText = roundedBmi;
+            
+            // show range and image based on BMI
+            if (bmi < 18.5) {
+                rangeElement.innerText = '(Underweight)';
+                stickerContainer.innerHTML = '<img id="sticker" class="sticker" src="img/underweight.png">';
+            } else if (bmi < 25) {
+                rangeElement.innerText = '(Normal weight)';
+                stickerContainer.innerHTML = '<img id="sticker" class="sticker" src="img/normalweight.png" style="margin-left: 2.2rem">';
+            } else if (bmi < 30) {
+                rangeElement.innerText = '(Overweight)';
+                stickerContainer.innerHTML = '<img id="sticker" class="sticker" src="img/overweight.png">';
+            } else {
+                rangeElement.innerText = '(Obesity)';
+                stickerContainer.innerHTML = '<img id="sticker" class="sticker" src="img/obesity.png" style="margin-right: 2.5rem">';
+            }
+        } else {
+            throw 'Enter a valid number';
+        }
+    } catch (error) {
+        alert(error);
     }
+});
+  const reloadButton = document.getElementById('reloadButton');
+  const reload = () => {
+      weightInput.value = '';
+      heightInput.value = '';
+      result.innerText = '00.0';
+      rangeElement.innerText = '___';
+      stickerContainer.innerHTML = '<img id="sticker" class="sticker" src="img/weight.png">';
+  };
+reloadButton.addEventListener('click', reload);
 
-    // Convert height from centimeters to meters
-    const heightm = height / 100; 
-    
-    // Calculate the BMI and round it to 1 decimal place
-    const bmi = weight / (heightm * heightm);
-    const roundeBmi = bmi.toFixed(1);
-
-    // Set the text of the result element to the calculated BMI only happen when the user has entered all the required inputs
-    if (height && weight) {
-      result.innerText = roundeBmi;
-    }
-
-
-
-
-// const imageWeight = document.getElementById('sticker');
-// imageWeight.style.opacity = 0;
-
-
-    if (bmi < 18.5) {
-        // const img1 = document.createElement('img')
-        // img1.src="img/save.png"
-        // imageContainer.appendChild(img1)
-        rangeElement.innerText = '(Underweight)'; 
-      } else if (bmi < 25) {
-        rangeElement.innerText = '(Normal weight)';
-      } else if (bmi < 30) {
-        rangeElement.innerText = '(Overweight)';
-      } else {
-        rangeElement.innerText = '(Obesity)';
-      }
 };
-
-
-
-
-
-
-
-
-
-
-const reloadButton = document.getElementById('reloadButton')
-const reload = () => {
-    weightInput.value = '';
-    heightInput.value = '';
-    result.innerText = '00.0';
-    rangeElement.innerText = '___';
-}
-reloadButton.onclick = reload;
-
-
-
-
-
-
-
-
-
 
 // Add event listeners to the input fields to trigger the calculateBmi function on input changes
 weightInput.addEventListener('input', calculateBmi);
 heightInput.addEventListener('input', calculateBmi);
 // 'input': This is the name of the event that we are listening for. we are listening for the input event, which is fired whenever the value of the weightInput element changes.
+
 
 
 
